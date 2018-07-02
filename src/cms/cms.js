8588c7c318b772dbbc2317bd58f2b1410db5c4a4
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-07-02 07:05:24 +0200

    Add image to content type post

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,11 @@
 import CMS from 'netlify-cms'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
+import SimplePagePreview from './preview-templates/SimplePagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewStyle('/styles.css');
+CMS.registerPreviewTemplate('imprint', SimplePagePreview);
+CMS.registerPreviewTemplate('dataPrivacy', SimplePagePreview);
+CMS.registerPreviewTemplate('products', ProductPagePreview);
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
