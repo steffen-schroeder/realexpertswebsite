187176ea59bef59f1e75b441a04df1fea2b19619
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-07-11 22:04:41 +0200

    Install and configure gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: `staticImages`,
         path: `${__dirname}/static/img`,
       },
     },
