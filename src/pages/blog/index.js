f2f86534f2a4f6e4c359c276bc00f4958c845108
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-07-11 22:12:03 +0200

    Create all markup for blog overview page and differ between top and all posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,43 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
+import React from 'react';
+import PropTypes from 'prop-types';
+import BlogPostTeaser from '../../components/BlogPostTeaser';
 
 export default class BlogIndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    // only consider the first three related posts on the front page as top posts
+    const topPosts = data.markdownRemark.fields.relatedPosts.slice(0,3);
+    // exclude posts from `all posts` which are already in top posts
+    const posts = data.allMarkdownRemark.edges
+      .filter(({ node: post }) => {
+        return !topPosts.find(topPost => topPost.id === post.id);
+      });
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
+      <section>
+        <h2>Top Beiträge</h2>
+        {topPosts.map((topPost, key) => (
+          <BlogPostTeaser key={topPost.id}
+                          post={topPost}
+                          type={key === 0 ? 'featured' : 'top'} />
+        ))}
+        <h2>Alle Beiträge</h2>
+        {posts.map(({ node: post }) => (
+          <BlogPostTeaser key={post.id}
+                          post={post}
+                          type='normal' />
+        ))}
       </section>
     )
   }
@@ -45,14 +34,59 @@ export default class BlogIndexPage extends React.Component {
 
 BlogIndexPage.propTypes = {
   data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      id: PropTypes.string,
+      fields: PropTypes.shape({
+        relatedPosts: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.string,
+          excerpt: PropTypes.string,
+          frontmatter: PropTypes.shape({
+            title: PropTypes.string,
+            date: PropTypes.string,
+            tags: PropTypes.arrayOf(PropTypes.string),
+          }),
+          fields: PropTypes.shape({
+            slug: PropTypes.string,
+            image: PropTypes.object,
+          }),
+        })),
+      }),
+    }),
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
+    # Query front page for related posts used as top posts
+    markdownRemark(fields: { slug: { eq: "/" }}) {
+      id
+      fields {
+        relatedPosts {
+          id
+          # TODO: Should we use _description_ instead?
+          excerpt(pruneLength: 400)
+          frontmatter {
+            title
+            date
+            tags
+          }
+          fields {
+            slug
+            image {
+              id
+              childImageSharp {
+                sizes (maxWidth: 850) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
+          }
+        }
+      }
+    }  
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] },
       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
@@ -63,14 +97,22 @@ export const pageQuery = graphql`
           id
           fields {
             slug
+            image {
+              childImageSharp {
+                sizes(maxWidth: 630) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
           }
           frontmatter {
             title
             templateKey
+            tags
             date(formatString: "MMMM DD, YYYY")
           }
         }
       }
     }
   }
-`
+`;
