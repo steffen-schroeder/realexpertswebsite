ffdb1e486ee39082bfcc37503903173f67cb603e
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-07-12 07:16:41 +0200

    Define tag overview page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,47 +1,33 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
+import BlogPostTeaser from '../components/BlogPostTeaser';
 
-class TagRoute extends React.Component {
+export default class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ))
-    const tag = this.props.pathContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
-    const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+    const posts = this.props.data.allMarkdownRemark.edges;
+    const tag = this.props.pathContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
+    const tagHeader = `${totalCount} ${
+      totalCount === 1 ? 'Beitrag' : 'Beiträge'
+    } in Kategorie “${tag}”`;
 
     return (
-      <section className="section">
+      <section>
         <Helmet title={`${tag} | ${title}`} />
-        <div className="container content">
-          <div className="columns">
-            <div
-              className="column is-10 is-offset-1"
-              style={{ marginBottom: '6rem' }}
-            >
-              <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-              <ul className="taglist">{postLinks}</ul>
-              <p>
-                <Link to="/tags/">Browse all tags</Link>
-              </p>
-            </div>
-          </div>
-        </div>
+        <small>{tagHeader}</small>
+        <h2>Alle Beiträge in <strong>{tag}</strong></h2>
+        {posts.map(({ node: post}) => (
+          <BlogPostTeaser post={post} type='normal' key={post.id} />
+        ))}
+        <p>
+          <Link to="/tags/">Alle Kategorien</Link>
+        </p>
       </section>
     )
   }
 }
-
-export default TagRoute
 
 export const tagPageQuery = graphql`
   query TagPage($tag: String) {
@@ -53,19 +39,31 @@ export const tagPageQuery = graphql`
     allMarkdownRemark(
       limit: 1000
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag] } } }
+      filter: { frontmatter: { tags: { in: [$tag] }, templateKey: { eq: "blog-post" } } }
     ) {
       totalCount
       edges {
         node {
+          excerpt(pruneLength: 400)
+          id
           fields {
             slug
+            image {
+              childImageSharp {
+                sizes(maxWidth: 630) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
           }
           frontmatter {
             title
+            templateKey
+            tags
+            date(formatString: "MMMM DD, YYYY")
           }
         }
       }
     }
   }
-`
+`;
