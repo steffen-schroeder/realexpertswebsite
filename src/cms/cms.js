dded10be7dd638f8923d3c2fe9b7e9080aa3def5
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-09-08 09:30:47 +0200

    Clean up unused templates and pages (products)

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,8 @@ import CMS from 'netlify-cms'
 
 import SimplePagePreview from './preview-templates/SimplePagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('imprint', SimplePagePreview);
 CMS.registerPreviewTemplate('dataPrivacy', SimplePagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
