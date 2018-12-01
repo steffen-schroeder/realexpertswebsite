c1c00b4833018ed1be2f9bc02227f18be970bed7
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-12-01 14:03:02 +0100

    Closes #58

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,14 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+            }
+          },
           'gatsby-remark-responsive-iframe',
         ],
       },
