1c20ba97452c28a26b3c0b677d0b0786b0ee91a3
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-12-03 20:19:26 +0100

    Closes #25

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import CookieConsent from "react-cookie-consent"
 import 'typeface-changa'
 import './all.scss'
 
@@ -13,6 +15,14 @@ const TemplateWrapper = ({ children }) => (
     <header id="header-main"><Navbar /></header>
     <main>{children()}</main>
     <Footer />
+    <CookieConsent
+      buttonText="Verstanden"
+      cookieName="reCoockieConsent"
+      style={{ background: "#40A6B9" }}
+      buttonStyle={{ background: "#fff", color: "#0E3F93", fontSize: "13px" }}>
+      Wir verwenden Cookies, um Ihnen einen optimalen Service zu bieten. Wenn Sie auf dieser Seite weitersurfen, stimmen Sie der Verwendung von Cookies zu.
+      Mehr zu <Link to="/datenschutz">Datenschutz</Link>.
+    </CookieConsent>
   </div>
 )
 
