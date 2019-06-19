bda8533d312ea845e7862a38e1a3da102463ae69
Steffen Schröder <schroeder@devid.net>
2019-06-19 14:15:48 +0200

    Modify success text

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Helmet from 'react-helmet';
 
 const ContactSuccessPage = () => (
   <Layout>
-    <div>
+    <section className="section contact">
+      <Helmet title={`Kontakt | Vielen Dank`}/>
       <h1>Vielen Dank!</h1>
-      <p>Vielen Dank für Ihre Kontaktanfrage. Wir werden uns umgehend mit Ihnen in Verbindung treten.</p>
-    </div>
+      <p>Wir haben Ihre Kontaktanfrage erhalten und werden uns umgehend mit Ihnen in Verbindung setzen.</p>
+    </section>
   </Layout>
 );
 
