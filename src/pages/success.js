5850b23b8e90107b68a4038b84bf1d81a52c54c5
Steffen Schröder <schroeder@devid.net>
2019-06-19 14:12:11 +0200

    Add success page

---
+++ src/pages/success.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const ContactSuccessPage = () => (
+  <Layout>
+    <div>
+      <h1>Vielen Dank!</h1>
+      <p>Vielen Dank für Ihre Kontaktanfrage. Wir werden uns umgehend mit Ihnen in Verbindung treten.</p>
+    </div>
+  </Layout>
+);
+
+export default ContactSuccessPage;
