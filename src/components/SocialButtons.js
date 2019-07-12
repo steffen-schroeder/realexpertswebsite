7988272a4db293b97caeabc58a9c58fa7edc1b77
Steffen Schröder <schroeder@devid.net>
2019-07-12 13:05:05 +0200

    Remoive OutboundLink from google-analytics

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, } from 'react-share';
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import Link from 'gatsby-link'
 
 const SocialButtons = ({ socialConfig, tags }) => (
   <div className="post-social">
-    <OutboundLink href={socialConfig.config.url}>
+    <Link href={socialConfig.config.url}>
       <FacebookShareButton url={socialConfig.config.url} className="facebook">
         <FacebookIcon size={48}
                       round={false}
                       iconBgStyle={{ fill: '#879fc9' }}
                       logoFillColor='white'/>
       </FacebookShareButton>
-    </OutboundLink>
-    <OutboundLink href={socialConfig.config.url}>
+    </Link>
+    <Link href={socialConfig.config.url}>
       <TwitterShareButton url={socialConfig.config.url} className="twitter" title={socialConfig.config.title}
                           via={socialConfig.twitterHandle.split('@').join('')} hashtags={tags}>
         <TwitterIcon size={48}
@@ -21,7 +21,7 @@ const SocialButtons = ({ socialConfig, tags }) => (
                      iconBgStyle={{ fill: '#879fc9' }}
                      logoFillColor='white'/>
       </TwitterShareButton>
-    </OutboundLink>
+    </Link>
   </div>
 );
 
