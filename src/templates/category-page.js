d0f6c125e80c282742d72a3cb3d4f9858df8de1e
Steffen Schröder <schroeder@devid.net>
2019-07-25 18:05:09 +0200

    Add or update templates

---
+++ src/templates/category-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default class CategoryIndexPage extends React.Component {
+  render() {
+
+    return (
+      <Layout>
+        <section className="category">
+          <h1>Kategorie</h1>
+        </section>
+      </Layout>
+    );
+  }
+}
---
+++ src/templates/portfolio-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default class PortfolioIndexPage extends React.Component {
+  render() {
+
+    return (
+      <Layout>
+        <section className="portfolio">
+          <h1>Portfolio</h1>
+        </section>
+      </Layout>
+    );
+  }
+}
