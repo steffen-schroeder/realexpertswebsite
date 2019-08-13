3dc4d373b703efc736d78d77b3b61c6cbd0e25da
Steffen Schröder <schroeder@devid.net>
2019-08-13 16:14:42 +0200

    Improve layout for success and 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Helmet from 'react-helmet';
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+  <Layout noHeader={true}>
+    <section className="section">
+      <Helmet title={`404 | Seite nicht gefunden`}/>
+      <div className="page-content">
+        <h2>Uups!</h2>
+        <div className="content-block-wrapper">
+          <p>Leider konnten wir die Seite nicht finden, haben Sie sich vielleicht verschrieben?</p>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
--- src/pages/success.js
+++ src/pages/success.js
@@ -3,11 +3,15 @@ import Layout from '../components/layout';
 import Helmet from 'react-helmet';
 
 const ContactSuccessPage = () => (
-  <Layout>
+  <Layout noHeader={true}>
     <section className="section contact">
       <Helmet title={`Kontakt | Vielen Dank`}/>
-      <h1>Vielen Dank!</h1>
-      <p>Wir haben Ihre Kontaktanfrage erhalten und werden uns umgehend mit Ihnen in Verbindung setzen.</p>
+      <div className="page-content">
+        <h2>Vielen Dank!</h2>
+        <div className="content-block-wrapper">
+          <p>Wir haben Ihre Kontaktanfrage erhalten und werden uns umgehend mit Ihnen in Verbindung setzen.</p>
+        </div>
+      </div>
     </section>
   </Layout>
 );
